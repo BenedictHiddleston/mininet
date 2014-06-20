aba00631db239b0dc3510f5a9afda79183d99958
crosson <cliff.rosson@gmail.com>
2014-06-20 10:53:45 -0700

    TopologySlice test

--- wk4/topologySlice.py
+++ wk4/topologySlice.py
@@ -37,12 +37,29 @@ class TopologySlice (EventMixin):
         # Use dpid to differentiate between switches (datapath-id)
         # Each switch has its own flow table. As we'll see in this 
         # example we need to write different rules in different tables.
-        dpid = dpidToStr(event.dpid)
+        dpid = int(dpidToStr(event.dpid))
         log.debug("Switch %s has come up.", dpid)
         
         """ Add your logic here """
+        if dpid == 1:
+            self.port_flows(event, (3,1))
+            '''
+            match = of.ofp_match()
+            match.in_port = 3
+            match.dl_dst = EthAddr(rule['pair'][1])
+            msg = of.ofp_flow_mod()
+            msg.match = match
+            msg.priority = rule['priority']
+            event.connection.send(msg)
+            '''
         
-
+    def port_flows(self, event, port_tuple):
+        match = of.ofp_match()
+        match.in_port = port_tuple[0]
+        msg = of.ofp_flow_mod()
+        msg.match = match
+        msg.action.append(of.ofp_action_output(port = port_tuple[1]))
+        event.connection.send(msg)
         
 
 def launch():
